72fe413f1bbdafd70331b1d1ebbda792beb243aa
Tatiana Frayman <alekseilier@ya.ru>
2021-04-20 23:52:25 +0300

    feat(category): category tests

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,31 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()
+            ->count(10)
+            ->create()
+            ->each(function (User $user) {
+            Post::factory()
+                ->count(random_int(4,12))
+                ->create([
+                "user_id" => $user->id
+            ])->each(function (Post $post) {
+                $post->categories()->sync(
+                    Category::inRandomOrder()
+                        ->take(random_int(1,4))
+                        ->get()
+                        ->pluck("id")
+                    );
+                });
+        });
     }
 }
